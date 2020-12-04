346173eaaef039ad7314697d0e1ee542a9502b74
Denys Robets <denysrobets@gmail.com>
2020-12-04 16:48:20 +0200

    preRelize test)

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,17 +1,4 @@
 $(document).ready(function () {
-	// $("#menu").on("click", "a", function (event) {
-	// 	//отменяем стандартную обработку нажатия по ссылке
-	// 	event.preventDefault();
-
-	// 	//забираем идентификатор бока с атрибута href
-	// 	var id = $(this).attr('href'),
-
-	// 		//узнаем высоту от начала страницы до блока на который ссылается якорь
-	// 		top = $(id).offset().top;
-
-	// 	//анимируем переход на расстояние - top за 1500 мс
-	// 	$('body,html').animate({ scrollTop: top }, 1500);
-	// });
 	$(window).resize(function () {
 		if ($(document).width() > 480) {
 			$("a.nav__link").on("click", function (e) {
@@ -23,5 +10,4 @@ $(document).ready(function () {
 			});
 		}
 	});
-
 });
