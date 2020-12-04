4ffb70dea9a029cc5e62b1ace855d193fe6e867b
Denys Robets <denysrobets@gmail.com>
2020-12-04 10:35:12 +0200

    test2

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -12,11 +12,13 @@ $(document).ready(function () {
 	// 	//анимируем переход на расстояние - top за 1500 мс
 	// 	$('body,html').animate({ scrollTop: top }, 1500);
 	// });
-	$("a.nav__link").on("click", function (e) {
-		e.preventDefault();
-		var anchor = $(this).attr('href');
-		$('html, body').stop().animate({
-			scrollTop: $(anchor).offset().top - 0
-		}, 800);
-	});
+	if ($(document).width() > 480) {
+		$("a.nav__link").on("click", function (e) {
+			e.preventDefault();
+			var anchor = $(this).attr('href');
+			$('html, body').stop().animate({
+				scrollTop: $(anchor).offset().top - 0
+			}, 800);
+		});
+	}
 });
