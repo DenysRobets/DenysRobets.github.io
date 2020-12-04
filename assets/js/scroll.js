f9dd0f45cd78619ee2c8c38a997cc57e11fb4202
Denys Robets <denysrobets@gmail.com>
2020-12-04 09:30:44 +0200

    test2

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
-	$("#menu").on("click", "a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+	// $("#menu").on("click", "a", function (event) {
+	// 	//отменяем стандартную обработку нажатия по ссылке
+	// 	event.preventDefault();
 
-		//забираем идентификатор бока с атрибута href
-		var id = $(this).attr('href'),
+	// 	//забираем идентификатор бока с атрибута href
+	// 	var id = $(this).attr('href'),
 
-			//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
+	// 		//узнаем высоту от начала страницы до блока на который ссылается якорь
+	// 		top = $(id).offset().top;
 
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({ scrollTop: top }, 1500);
+	// 	//анимируем переход на расстояние - top за 1500 мс
+	// 	$('body,html').animate({ scrollTop: top }, 1500);
+	// });
+	$("a.nav__link").on("click", function (e) {
+		e.preventDefault();
+		var anchor = $(this).attr('href');
+		$('html, body').stop().animate({
+			scrollTop: $(anchor).offset().top - 0
+		}, 800);
 	});
 });
